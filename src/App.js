de1ac8d6e13ac53ac566c9b0df02db7430c07024
ahipsharma <76726757+ahipsharma@users.noreply.github.com>
2023-04-08 20:18:27 +0530

    Landing Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Hero from './components/hero';
+import Slider from './components/Slider';
+import Navbar from './components/Navbar';
+
+import travel_01 from './assets/travel-01.jpg';
+import travel_02 from './assets/travel-02.jpg';
+import travel_03 from './assets/travel-03.jpg';
+
+const navbarLinks = [
+  {url: "#", title: "Home"}, 
+  {url: "#", title: "Trips"}, 
+  {url: "#", title: "Rewards"}
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar navbarLinks={navbarLinks}/>
+      <Hero imageSrc={travel_01}/>
+      <Slider 
+        imageSrc={travel_02} 
+        title={"Be an explorer"} 
+        subtitle={"Our platform offers many features"}
+        flipped={false}
+      />
+      <Slider 
+        imageSrc={travel_03} 
+        title={"Be an explorer"} 
+        subtitle={"Our platform offers many features"}
+        flipped={true}
+      />
     </div>
   );
 }
