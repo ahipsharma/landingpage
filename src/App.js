8949b6be7604711246e2308fcd7fc13849a45540
ahipsharma <76726757+ahipsharma@users.noreply.github.com>
2023-04-26 14:17:06 +0530

    Landing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Navbar from './components/Navbar';
 import travel_01 from './assets/stock.jpeg';
 import travel_02 from './assets/ent.jpeg';
 import travel_03 from './assets/phone.jpeg';
+import travel_04 from './assets/1.jpeg';
+import travel_05 from './assets/2.jpeg';
+import travel_06 from './assets/3.jpeg';
 
 const navbarLinks = [
   {url: "#", title: "Home"}, 
@@ -18,16 +21,34 @@ function App() {
     <div className="App">
       <Navbar navbarLinks={navbarLinks}/>
       <Hero imageSrc={travel_01}/>
-      <Slider 
+      {/* <Slider 
         imageSrc={travel_02} 
         title={"Be an explorer"} 
-        subtitle={"Our platform offers many features"}
+        // subtitle={"Our platform offers many features"}
         flipped={false}
       />
       <Slider 
         imageSrc={travel_03} 
         title={"Be an explorer"} 
-        subtitle={"Our platform offers many features"}
+        // subtitle={"Our platform offers many features"}
+        flipped={true} */}
+      />
+      <Slider 
+        imageSrc={travel_04} 
+        title={"Invest with Confidence"} 
+        // subtitle={"Our platform offers many features"}
+        flipped={true}
+      />
+      <Slider 
+        imageSrc={travel_05} 
+        title={"Your Stock Companion"} 
+        // subtitle={"Our platform offers many features"}
+        flipped={false}
+      />
+      <Slider 
+        imageSrc={travel_06} 
+        title={"Get market insights"} 
+        // subtitle={"Our platform offers many features"}
         flipped={true}
       />
     </div>
